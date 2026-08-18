997f44d9200b8e31a671742a74144138fb95632d
Sathvik-nissy <kotapurisathvik@gmail.com>
2026-08-18 09:36:46 +0530

    Time: 77 ms (32.62%), Space: 47.9 MB (43.84%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int  sum=0;
+        for(int x:nums){
+            sum=sum+x;
+        }
+        if(sum%2!=0)return false;
+        int target=sum/2;
+        return knap(target,nums);    
+    }
+
+    static boolean knap(int W, int wt[]) {
+        int n=wt.length;
+        boolean dp[][]=new boolean[n+1][W+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=W;j++){
+                if(j==0){
+                    dp[i][j]=true;
+                }else{
+                    boolean p=false;
+                    if(wt[i-1]<=j){
+                        p=dp[i-1][j]||dp[i-1][j-wt[i-1]];
+                    }
+                    boolean np=dp[i-1][j];
+                    dp[i][j]=np||p;
+                }
+                
+            }
+        }
+        return dp[n][W];
+
+    }   
+}
